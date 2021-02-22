c3b47ed8f0b7a4a0b045fa2150fe736cf070fddb
William Tyler Pelt <wtpelt@gmail.com>
2021-02-21 21:53:15 -0500

    Made several styling updates/corrections; Added current page title for mobile header; Updated the navbar collapse break point; Added placeholder About page.

--- music/src/app/header/header.component.ts
+++ music/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +8,29 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  myUrl: any;
+  title: string;
+
   constructor(private router: Router, private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+  )
+      .subscribe(event => {
+          this.myUrl = event;
+          if(this.myUrl.url === "/music"){
+            this.title = 'Music'
+          }
+          else if (this.myUrl.url === "/about"){
+            this.title = 'About'
+          }
+          else if (this.myUrl.url === "/"){
+            this.title = 'Home'
+          }
+          else {
+            this.title = ''
+          }
+      });
+ }
 
 }

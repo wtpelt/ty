2444eb88ecd652a49fe4d4fc63328a6cc357c012
William Tyler Pelt <42362488+wtpelt@users.noreply.github.com>
2023-06-17 00:01:06 -0400

    My commmit comments won't be accurate here, because I had some issue pushing large cache files to github, and since I had ten commits ready to push all at once after Angular upgrades, I had to revert until I could revert and remove those large cache files. All in all, tonight I updated Angular from 11 to 16, remove a ton of unused CSS code, and decreased breakpoint for mobile menu.

--- music/src/app/music/album-one/album-one.component.ts
+++ music/src/app/music/album-one/album-one.component.ts
@@ -5,7 +5,6 @@ import { otherPlatformsButtonText } from 'src/app/shared/button-label';
 @Component({
   selector: 'app-album-one',
   templateUrl: './album-one.component.html',
-  // styleUrls: ['./music.component.scss']
 })
 export class AlbumOneComponent {
   constructor(private router: Router, private route: ActivatedRoute) {}
--- music/src/app/music/album-two/album-two.component.ts
+++ music/src/app/music/album-two/album-two.component.ts
@@ -5,7 +5,6 @@ import { otherPlatformsButtonText } from 'src/app/shared/button-label';
 @Component({
   selector: 'app-album-two',
   templateUrl: './album-two.component.html',
-  // styleUrls: ['./music.component.scss']
 })
 export class AlbumTwoComponent {
   constructor(private router: Router, private route: ActivatedRoute) {}

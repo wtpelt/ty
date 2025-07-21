2b638942f27a4e8a9c0070a997f5a2e36a8b92f7
William Tyler Pelt <42362488+wtpelt@users.noreply.github.com>
2025-07-20 21:39:14 -0400

    New album updates, mainly.

--- music/src/app/home/home.component.ts
+++ music/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { otherPlatformsButtonText } from '../shared/button-label';
 
 @Component({
@@ -9,5 +9,34 @@ import { otherPlatformsButtonText } from '../shared/button-label';
 export class HomeComponent {
   title = 'home';
   showSection = false;
+  showBackToTop = false;
   otherPlatformsButtonText = otherPlatformsButtonText;
+  isMobile = false;
+
+  constructor() {
+    // Initial check
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    // Show button when page is scrolled more than 400px
+    this.showBackToTop = window.scrollY > 400;
+  }
+
+  private checkScreenSize() {
+    this.isMobile = window.innerWidth <= 1000;
+  }
+
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
 }
